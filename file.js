17f4c71321424bdb784c18264e09cde6fc981973
Liubov Minakova <msnakova7@gmail.com>
2021-07-10 22:26:30 +0200

    no message

--- file.js
+++ file.js
@@ -1,8 +1,80 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+document.querySelector('.choice__btn').addEventListener('click', () => {
 
-\f0\fs24 \cf0 \uc0\u1088 \u1074 \u1083 \u1099 }
+
+  const field = document.querySelector('.container')
+  field.classList.add('clear')
+
+
+  const table = document.createElement('div');
+  table.className = 'table';
+  document.body.append(table);
+  const game = document.querySelector('.table');
+
+  function getCards(a) {
+    for(let i=1; i<=a; i++) {
+      let div = document.createElement('div');
+      div.className = 'card';
+      game.appendChild(div);
+    }
+  }
+
+  function getCovers() {
+    let cards = document.querySelectorAll('.card');
+    cards.forEach(function(card, index) {
+      let cover = document.createElement('div');
+      cover.className = 'cover';
+      card.append(cover);
+    })
+  }
+
+  function openCard() {
+    let cards = document.querySelectorAll('.card');
+    let random = Math.floor(Math.random() * cards.length);
+    let bug = cards[random];
+
+    let cardFlipped = false;
+
+    for (let i = 0; i < cards.length; i++) {
+      function rotate() {
+        if (cards[i] == bug) {
+          cards[i].classList.add('bug');
+        } else {
+            cards[i].classList.add('gameOver');
+        }
+      }
+
+      cards[i].addEventListener('click', () => {
+        if (cardFlipped == true) {
+          window.location.reload();
+        } else {
+            rotate();
+            cardFlipped = true;
+        }
+      })
+    }
+  }
+
+  const levels = document.querySelectorAll('.choice__radio');
+  for (let i=0; i < levels.length; i++) {
+    if (levels[i].checked) {
+      let value = levels[i].value;
+
+      if (value === 'easy') {
+        getCards(3);
+        game.classList.add('table3Cards');
+      }
+
+      if (value === 'medium') {
+        getCards(6);
+      }
+
+      if (value === 'hard') {
+        getCards(10);
+        game.classList.add('table10Cards');
+      }
+
+    getCovers();
+    openCard();
+    }
+  }
+})
